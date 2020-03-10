435b9756d554752eb65341e9e8ca35bb0d0cfc9f
Tim Cera <tim@cerazone.net>
2020-03-10 07:32:49 -0400

    CI: Normalize to tstoolbox.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
-import shutil
 import sys
 
 from setuptools import setup
@@ -15,32 +17,16 @@ version = open("VERSION").readline().strip()
 
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist")
-
-    # The following block of code is to set the timestamp on files to
-    # 'now', otherwise ChromeOS/google drive sets to 1970-01-01 and then
-    # no one can install it because zip doesn't support dates before
-    # 1980.
-    os.chdir("dist")
-    os.system("tar xvzf {pkg_name}-{version}.tar.gz".format(**locals()))
-    os.system("find {pkg_name}-{version}* -exec touch {{}} \\;".format(**locals()))
-    os.system(
-        "tar czf {pkg_name}-{version}.tar.gz {pkg_name}-{version}".format(**locals())
-    )
-    shutil.rmtree("{pkg_name}-{version}".format(**locals()))
-    os.chdir("..")
-
     os.system("twine upload dist/{pkg_name}-{version}.tar.gz".format(**locals()))
     sys.exit()
 
-README = open("./README.rst").read()
+README = open("README.rst").read()
 
 install_requires = [
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
     "tstoolbox",
     "hspfbintoolbox",
 ]
-
-setup_requires = []
 
 setup(
     name=pkg_name,
@@ -58,19 +44,20 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords="hydrology",
+    ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords="time_series",
     author="Tim Cera, P.E.",
     author_email="tim@cerazone.net",
     url="http://timcera.bitbucket.io/{pkg_name}/docsrc/index.html".format(**locals()),
     packages=[pkg_name],
     include_package_data=True,
     zip_safe=False,
-    setup_requires=setup_requires,
     install_requires=install_requires,
     entry_points={
         "console_scripts": ["{pkg_name}={pkg_name}.{pkg_name}:main".format(**locals())]
